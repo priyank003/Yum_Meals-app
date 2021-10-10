08789660c5068af66e9063b3e1e18f2b6490f3d7
Priyank <priyankcrjr7@gmail.com>
2021-10-10 20:31:32 +0530

    added sidenav redux

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -4,51 +4,65 @@ import fb from "../../assets/contact/fb.png";
 import watsapp from "../../assets/contact/watsapp.png";
 import tweet from "../../assets/contact/twitter.png";
 import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { sideNavActions } from "../../store/sideNav-slice";
+import { useDispatch, useSelector } from "react-redux";
 const SideNav = (props) => {
+  const dispatch = useDispatch();
+
+  const sideNavHandler = useSelector((state) => state.sideNav.show);
+  const hideSideNavHandler = () => {
+    dispatch(sideNavActions.hideSideNav());
+  };
   return (
     <div className={classes["sideNav-wrap"]}>
-      {props.hamMenu && (
-        <div className={classes.sideNav}>
-          <div className={classes.login}>
-            <Link to="/login">
-              <Button>Login</Button>
-            </Link>
-            {}
-            <span>or</span>
-            <Button>Sign-up</Button>
-          </div>
-          <div className={classes["sideNav-links"]}>
-            <div className={classes["nav-links"]}>
-              <a href="/why">Why Yum Meals ?</a>
-              <Link to="/">
-                <a href="/services">Services</a>
+      {sideNavHandler && (
+        <React.Fragment>
+          <div
+            className={classes["sidenav-backdrop"]}
+            onClick={hideSideNavHandler}
+          />
+          <div className={classes.sideNav}>
+            <div className={classes.login}>
+              <Link to="/login">
+                <Button>Login</Button>
               </Link>
+              {}
+              <span>or</span>
+              <Button>Sign-up</Button>
+            </div>
+            <div className={classes["sideNav-links"]}>
+              <div className={classes["nav-links"]}>
+                <a href="/why">Why Yum Meals ?</a>
+                <Link to="/">
+                  <a href="/services">Services</a>
+                </Link>
 
-              <Link to="/menu">
-                <a href="/menu">Menu</a>
-              </Link>
+                <Link to="/menu">
+                  <a href="/menu">Menu</a>
+                </Link>
 
-              <a href="/contact">Locate Store</a>
+                <a href="/contact">Locate Store</a>
+              </div>
+
+              <div className={classes["nav-contact"]}>
+                <a href="/www">Contact Us</a>
+                <a href="/kjk">About us</a>
+              </div>
             </div>
 
-            <div className={classes["nav-contact"]}>
-              <a href="/www">Contact Us</a>
-              <a href="/kjk">About us</a>
+            <div className={classes["nav-brand"]}>
+              <h1>
+                Yum <span>Meals</span>
+              </h1>
+              <div className={classes["social-icons"]}>
+                <img src={fb} alt="" />
+                <img src={watsapp} alt="" />
+                <img src={tweet} alt="" />
+              </div>
             </div>
           </div>
-
-          <div className={classes["nav-brand"]}>
-            <h1>
-              Yum <span>Meals</span>
-            </h1>
-            <div className={classes["social-icons"]}>
-              <img src={fb} alt="" />
-              <img src={watsapp} alt="" />
-              <img src={tweet} alt="" />
-            </div>
-          </div>
-        </div>
+        </React.Fragment>
       )}
     </div>
   );
