c9a3e4c95d38dc7ee8d966598f67ffcc2195ee9a
Priyank <priyankcrjr7@gmail.com>
2021-10-10 20:32:04 +0530

    wrapped with provider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
