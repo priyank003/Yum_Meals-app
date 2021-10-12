3acbe8b5ae41c075d98dbd58f7ffc900080d9135
Priyank <priyankcrjr7@gmail.com>
2021-10-12 20:44:17 +0530

    added cartitems reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // import authSlice from "./auth-slice";
 import sideNavSlice from "./sideNav-slice";
-
+import cartSlice from "./cartItem-slice";
 const store = configureStore({
   reducer: {
     // auth: authSlice.reducer,
     sideNav: sideNavSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
